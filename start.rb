52d20adccd12cd3fa19d6e93e4260af04ace65f2
Joe Hsu <jhsu.x1@gmail.com>
2015-06-19 10:15:04 -0700

    contribute tracks to group

--- start.rb
+++ start.rb
@@ -13,6 +13,11 @@ AUTH = {client_id: ENV['SOUNDCLOUD_AUTH_CLIENT_ID'],
 
 @users = ["chunkybeats25", "jhsu", "nlaf"]
 
+# channel id => name cache
+@channels = {}
+@group_url = "https://soundcloud.com/groups/coolpeeps-music"
+@group_id = nil
+
 @client = SoundCloud.new({
   client_id: AUTH[:client_id],
   client_secret: AUTH[:client_secret],
@@ -38,8 +43,7 @@ end
 # ie:
 #     who_likes("...")
 #     #=> ["jhsu"]
-def who_likes(url)
-  track = resolve_url(url)
+def who_likes(track)
   if track && track.kind == "track"
     @users.map {|username|
       if get_liked(username, track.id)
@@ -51,25 +55,60 @@ def who_likes(url)
   end
 end
 
+def contribute_to_group(client, group_id, track)
+  client.post("/groups/#{group_id}/contributions", {track: track.id})
+end
+
+# Send a message to a channel.
+#
+# channel - a string channel name (without '#')
+# message - a string message
+def send_message(channel, message)
+  HTTP.post(INCOMING_URL, json: {
+    username: "soundlikes",
+    channel: "##{channel}",
+    text: message
+  })
+end
+
+def get_channel_name(channel_data)
+  return @channels[channel_data] if @channels[channel_data]
+
+  channel_response = HTTP.get("https://slack.com/api/channels.info",
+                              params: {token: USER_TOKEN, channel: channel_data})
+  channel_info = JSON.parse(channel_response.body)['channel']
+  @channels[channel_data] = channel_info['name']
+end
+
 @rtmclient = SlackRTM::Client.new(websocket_url: RTM_URL)
 @rtmclient.on(:message) do |data|
   if data["type"] == "message" && !data["hidden"]
     urls = data["text"].scan(/https:\/\/soundcloud\.com\/[^\/\s]+\/[^\/\s>]+/i)
     if urls.any?
-      channel_response = HTTP.get("https://slack.com/api/channels.info", params: {token: USER_TOKEN, channel: data['channel']})
-      channel_info = JSON.parse(channel_response.body)['channel']
-      channel_name = channel_info['name']
+      # set the group_id if not set yet and there's a group_url
+      if @group_url && !@group_id && group = resolve_url(@group_id)
+        @group_id = group.id
+      end
+
+      channel_name = get_channel_name(data['channel'])
       urls.each do |url|
-        usernames = who_likes(url)
+        track = resolve_url(url)
+
+        # contribute track to the group
+        if track.kind == "track"
+          contribute_to_group(@client, @group_id, track)
+        end
+
+        usernames = who_likes(track)
         if usernames.any?
-          payload = {
-            username: "soundlikes",
-            channel: "##{channel_name}",
-            text: "Liked by users: #{usernames.join(", ")}"
-          }
-          HTTP.post(INCOMING_URL, json: payload)
+          send_message(channel_name, "Liked by users: #{usernames.join(", ")}")
+        else
+          send_message(channel_name, "No one liked that track")
         end
       end
+    elsif !(dasta["text"] =~ /\A\s*songs/).nil?
+      channel_name = get_channel_name(data['channel'])
+      send_message(channel_name, @group_url)
     end
   end
 end
